9e74696b70ef68b733f23c28246fe78a7809cac8
yogesh021 <yogeshtripathi273@gmail.com>
2021-02-23 16:58:47 +0530

    change RSS URL

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     createProxyMiddleware("/ndtvnews-trending-news", {
-      target: "http://feeds.feedburner.com",
+      target: "https://feeds.feedburner.com",
       secure: false,
       changeOrigin: true,
     })
